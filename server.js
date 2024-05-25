8f475a80da34045c24dedb8a8decb29e2b7c5d00
Kauê Matheus <kauematheus7@hotmail.com>
2024-05-25 13:19:31 -0300

    req, res
    request e resposta, especificando e analisando como funciona

--- server.js
+++ server.js
@@ -2,9 +2,22 @@ import express from 'express'
 
 const app = express()
 
+const users = []
+
+
+app.post('/users', (req, res) => {
+    
+    console.log(req)
+
+    res.send('ok post, deu certo')
+
+})
+
+
 app.get('/users', (req, res) => {
     res.send('ok, deu bom')  
 })
 
 
 app.listen(3000)
+
